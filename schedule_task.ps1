7df2d01968ae4751015a1cf9599c4c06f542f23c
Breno Rios <breno@brenorios.com.br>
2023-06-22 11:05:04 -0300

    Changed name of scheduled task on schedule_task.ps1

--- schedule_task.ps1
+++ schedule_task.ps1
@@ -2,4 +2,4 @@ $delay = New-TimeSpan -Minutes 3
 $action = New-ScheduledTaskAction -Execute 'python.exe' -Argument $pwd"\domainReminder.py"
 $trigger = New-ScheduledTaskTrigger -AtLogOn
 $trigger.Delay = 'PT3M'
-$task = Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "TestTask" -Description "Verifica a renovação dos domínios na inicialização do sistema"
+$task = Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Domain Reminder" -Description "Verifica a renovação dos domínios na inicialização do sistema"
